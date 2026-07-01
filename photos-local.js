79cebc19fd7cb07a0e8e0f3ebc16e04af4d57457
Chad Burmeister <chadburmeister@gmail.com>
2026-07-01 16:42:02 +0000

    Rebuild Photos page: event albums + search + iCloud Shared Album pull (api/photos)

---
+++ photos-local.js
@@ -0,0 +1,116 @@
+window.LOCAL_EVENTS = [
+  {
+    "id": "local-favorites",
+    "title": "Family Favorites",
+    "date": "2018\u20132026",
+    "source": "seed",
+    "cover": "photos/01.jpg",
+    "count": 13,
+    "photos": [
+      {
+        "url": "photos/01.jpg",
+        "w": null,
+        "h": null,
+        "caption": "",
+        "contributor": "The Burmeisters",
+        "date": null
+      },
+      {
+        "url": "photos/02.jpg",
+        "w": null,
+        "h": null,
+        "caption": "",
+        "contributor": "The Burmeisters",
+        "date": null
+      },
+      {
+        "url": "photos/03.jpg",
+        "w": null,
+        "h": null,
+        "caption": "",
+        "contributor": "The Burmeisters",
+        "date": null
+      },
+      {
+        "url": "photos/04.jpg",
+        "w": null,
+        "h": null,
+        "caption": "",
+        "contributor": "The Burmeisters",
+        "date": null
+      },
+      {
+        "url": "photos/05.jpg",
+        "w": null,
+        "h": null,
+        "caption": "",
+        "contributor": "The Burmeisters",
+        "date": null
+      },
+      {
+        "url": "photos/06.jpg",
+        "w": null,
+        "h": null,
+        "caption": "",
+        "contributor": "The Burmeisters",
+        "date": null
+      },
+      {
+        "url": "photos/07.jpg",
+        "w": null,
+        "h": null,
+        "caption": "",
+        "contributor": "The Burmeisters",
+        "date": null
+      },
+      {
+        "url": "photos/08.jpg",
+        "w": null,
+        "h": null,
+        "caption": "",
+        "contributor": "The Burmeisters",
+        "date": null
+      },
+      {
+        "url": "photos/09.jpg",
+        "w": null,
+        "h": null,
+        "caption": "",
+        "contributor": "The Burmeisters",
+        "date": null
+      },
+      {
+        "url": "photos/10.jpg",
+        "w": null,
+        "h": null,
+        "caption": "",
+        "contributor": "The Burmeisters",
+        "date": null
+      },
+      {
+        "url": "photos/11.jpg",
+        "w": null,
+        "h": null,
+        "caption": "",
+        "contributor": "The Burmeisters",
+        "date": null
+      },
+      {
+        "url": "photos/12.jpg",
+        "w": null,
+        "h": null,
+        "caption": "",
+        "contributor": "The Burmeisters",
+        "date": null
+      },
+      {
+        "url": "photos/13.jpg",
+        "w": null,
+        "h": null,
+        "caption": "",
+        "contributor": "The Burmeisters",
+        "date": null
+      }
+    ]
+  }
+];
